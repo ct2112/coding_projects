425846321d99892d9b9a991021804650ea5edee8
Cturtel <cturtel22@gmail.com>
2021-06-16 17:37:03 -0600

    threading works, oop is implemented just need to work on counting down player times but turns switch and everything

--- catan_timer/main.py
+++ catan_timer/main.py
@@ -1,53 +1,17 @@
 from curses import wrapper
 from player import player, game
-import sys
-import time
-from datetime import datetime
-import threading
-from math import floor
-
-screen = 0
-def display_thread(scr):
-
-	game_start = int(time.time())
-	while 1:
-		scr.clear()
-		time_elapsed = int(time.time()) - game_start
-		scr.addstr(0,0, "total Time elapsed - {:d}:{:02d}".format(int(time_elapsed/60), time_elapsed % 60))
-		for player in players:
-			scr.addstr(player.id * 2, 0, str(player))
-		scr.refresh()
-		time.sleep(1)
-
-def timer_func():
-	prev_turn = game.cur_turn
-	cur_turn_time = 0
-	while 1:
-		if prev_turn != game.cur_turn:
-			cur_turn_time = int(time.time())
-
-		players[game.cur_turn].time_left
-		
+import sys		
 
 def main(scr):
     # Clear screen
-	game = game(players, scr)
-	game.start()
-	disp_thread = threading.Thread(target=display_thread, args=[scr])
-	timer_thread= threading.Thread(target=timer_func)
-	game_in_prog = 1
-	scr.refresh()
-	disp_thread.start()
-	timer_thread.start()
-	while game_in_prog:
-		scr.getch()
-		game.cur_turn = (game.cur_turn + 1) % len(players)
-
+	game.scr = scr
+	game.setup()
+	game.play()
 	
 if __name__ == "__main__":
 	time_to_start_with = sys.argv[1] #time in minutes per player
 	players = []
 	for i, name in enumerate(sys.argv[2:]):
 		players.append(player(name, int(time_to_start_with), i + 1 ))
-	
+	game = game(players)
 	wrapper(main)
--- catan_timer/player.py
+++ catan_timer/player.py
@@ -1,4 +1,6 @@
 from math import floor
+import time
+import threading
 
 class player:
 	def __init__(self, inp_name, init_time, inp_id):
@@ -9,24 +11,62 @@ class player:
 		return "{:d}:{:12} - {:02d}:{:02d}".format(self.id, self.name,floor(self.time_left/60), floor(self.time_left%60))
 
 class game:
-	def __init__(self, inp_players, inp_scr):
+	def __init__(self, inp_players):
 		self.time_elapsed = 0
 		self.cur_turn = 0
 		self.cur_turn_time = 0
 		self.in_progress = 0
 		self.players = inp_players
-		self.scr = inp_scr
 
-	def start(self, scr):
-		scr.clear()
+	def setup(self):
+		self.scr.clear()
 		num_players = len(self.players)
-		scr.addstr("there are {} players:\n".format(num_players))
+		self.scr.addstr("there are {} players:\n".format(num_players))
 		for i, play in enumerate(self.players):
-			scr.addstr(i,0,"{}:{}".format(i, play))
-		scr.addstr("\npress any key to start")
-		scr.refresh()
-		scr.getch()
-		scr.clear()		
+			self.scr.addstr(i,0,"{}".format(play))
+		self.scr.addstr("\npress any key to start")
+		self.scr.refresh()
+		self.scr.getch()
+		self.scr.clear()
+
+	def play(self):
+		disp_thread  = threading.Thread(target = self.display_thread, daemon=True)
+		timer_thread = threading.Thread(target = self.timer_func, daemon=True)
+		self.in_progress = 1
+
+		disp_thread.start()
+		timer_thread.start()
+		while self.in_progress:
+			#if self.scr.getch() == KEY_BACKSPACE:
+			#	self.in_progress = 0
+			#	self.scr.getch()
+			#	self.in_progress = 1
+			self.scr.getch()
+			self.cur_turn = (self.cur_turn + 1) % len(self.players)
+		
+	def display_thread(self):
+		game_start = int(time.time())
+		while 1:
+			self.scr.clear()
+			time_elapsed = int(time.time()) - game_start
+			self.scr.addstr(0,0, "total Time elapsed - {:d}:{:02d}\n".format(int(time_elapsed/60), time_elapsed % 60))
+			self.scr.addstr("{}'s turn".format(self.players[self.cur_turn].name))
+			for player in self.players:
+				self.scr.addstr(player.id * 2 + 1, 0, str(player))
+			self.scr.refresh()
+			time.sleep(1)
+
+	def timer_func(self):
+		pass
+		"""
+		prev_turn = self.cur_turn
+		cur_turn_time = 0
+		while 1:
+			if prev_turn != self.cur_turn:
+				cur_turn_time = int(time.time())
+			self.players[self.cur_turn].time_left
+			time.sleep(1)
+			"""		
 if __name__ == "__main__":		
 	temp = player("casey", 15)
 	print(temp)
