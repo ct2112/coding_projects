89b75674b02fb7050385d415dafebb5f53645c8e
Cturtel <cturtel22@gmail.com>
2021-06-16 20:22:25 -0600

    fully functional without pause, or increment

--- catan_timer/player.py
+++ catan_timer/player.py
@@ -15,6 +15,7 @@ class game:
 		self.time_elapsed = 0
 		self.cur_turn = 0
 		self.cur_turn_time = 0
+		self.num_turns = 0
 		self.in_progress = 0
 		self.players = inp_players
 
@@ -43,6 +44,8 @@ class game:
 			#	self.in_progress = 1
 			self.scr.getch()
 			self.cur_turn = (self.cur_turn + 1) % len(self.players)
+			if self.cur_turn == 0:
+				self.num_turns = self.num_turns + 1
 		
 	def display_thread(self):
 		game_start = int(time.time())
@@ -50,23 +53,24 @@ class game:
 			self.scr.clear()
 			time_elapsed = int(time.time()) - game_start
 			self.scr.addstr(0,0, "total Time elapsed - {:d}:{:02d}\n".format(int(time_elapsed/60), time_elapsed % 60))
-			self.scr.addstr("{}'s turn".format(self.players[self.cur_turn].name))
+			self.scr.addstr("{}'s turn {:d}".format(self.players[self.cur_turn].name, self.num_turns))
 			for player in self.players:
 				self.scr.addstr(player.id * 2 + 1, 0, str(player))
 			self.scr.refresh()
 			time.sleep(1)
 
 	def timer_func(self):
-		pass
-		"""
-		prev_turn = self.cur_turn
-		cur_turn_time = 0
+		prev_turn = 0
+		orig_turn_time = self.players[0].time_left
+		cur_turn_time = int(time.time())
 		while 1:
 			if prev_turn != self.cur_turn:
 				cur_turn_time = int(time.time())
-			self.players[self.cur_turn].time_left
+				orig_turn_time = self.players[self.cur_turn].time_left
+				prev_turn = self.cur_turn
+			self.players[self.cur_turn].time_left = orig_turn_time - (int(time.time()) - cur_turn_time)
 			time.sleep(1)
-			"""		
+				
 if __name__ == "__main__":		
 	temp = player("casey", 15)
 	print(temp)
